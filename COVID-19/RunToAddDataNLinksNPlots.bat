e38621c9f42b1815a1b9bfb58cab47091dc11d3a
Saideep Gajula <SGajula@slb.com>
2020-07-05 00:48:07 +0530

    Push to add Octave running into bat file

--- COVID-19/RunToAddDataNLinksNPlots.bat
+++ COVID-19/RunToAddDataNLinksNPlots.bat
@@ -3,10 +3,10 @@ echo script to RunToAddDataNLinksNPlots STARTED at %time:~0,2%hrs%time:~3,2%min%
 set todaydatetime=%time:~0,2%hrs%time:~3,2%min%time:~6,2%secs_%date:~-7,2%-%date:~-10,2%-%date:~-4,4%
 python "C:\Users\SGajula\NotBackedUp\Me\Learn\GitHub\deepuHub\COVID-19\adddatafortoday.py" > "Logs\file_adddata_%todaydatetime%.txt"
 python "C:\Users\SGajula\NotBackedUp\Me\Learn\GitHub\deepuHub\COVID-19\addtodaylinks.py" > "Logs\file_addlinks_%todaydatetime%.txt"
-rem timeout 10
-rem octave --no-gui "c:\users\sgajula\notbackedup\me\learn\github\deepuhub\covid-19\plotcovid2019.m"
-rem timeout 20
-rem octave --no-gui "c:\users\sgajula\notbackedup\me\learn\github\deepuhub\covid-19\plottotalvsnewperday.m"
+pause
+octave --no-gui "c:\users\sgajula\notbackedup\me\learn\github\deepuhub\covid-19\plotcovid2019.m"
+pause
+octave --no-gui "c:\users\sgajula\notbackedup\me\learn\github\deepuhub\covid-19\plottotalvsnewperday.m"
 pause
 echo script to RunToAddDataNLinksNPlots ENDED at %time:~0,2%hrs%time:~3,2%min%time:~6,2%secs
 set commitdatetime=%time:~0,2%hrs%time:~3,2%min%time:~6,2%secs_%date:~-7,2%-%date:~-10,2%-%date:~-4,4% 
