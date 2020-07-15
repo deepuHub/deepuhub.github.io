b8dbcee5010e18fb2709b1708075114cd340205c
Saideep Gajula <SGajula@slb.com>
2020-07-15 17:24:54 +0530

    update timeouts in auto run

--- COVID-19/RunToAddDataNLinksNPlotsNPushToGit.bat
+++ COVID-19/RunToAddDataNLinksNPlotsNPushToGit.bat
@@ -5,7 +5,7 @@ echo Adding Data for Today
 python "C:\Users\SGajula\NotBackedUp\Me\Learn\GitHub\deepuHub\COVID-19\adddatafortoday.py" > "Logs\file_adddata_%todaydatetime%.txt"
 echo Adding Links for Today
 python "C:\Users\SGajula\NotBackedUp\Me\Learn\GitHub\deepuHub\COVID-19\addtodaylinks.py" > "Logs\file_addlinks_%todaydatetime%.txt"
-timeout 20
+timeout 15
 echo Generating Plots - Summary
 octave --no-gui "c:\users\sgajula\notbackedup\me\learn\github\deepuhub\covid-19\plotcovid2019.m"
 echo Generating Plots - Total vs New
@@ -16,12 +16,12 @@ set commitdatetime=%time:~0,2%hrs%time:~3,2%min%time:~6,2%secs_%date:~-7,2%-%dat
 echo commitdatetime is %commitdatetime%
 @echo on
 git status
-timeout 15
+timeout 10
 git add .
-timeout 15
+timeout 10
 git commit -a -m "Auto-push for %commitdatetime%"
-timeout 30
+timeout 15
 git push origin master
-timeout 45
+timeout 10
 git status
-timeout 5
+timeout 7
