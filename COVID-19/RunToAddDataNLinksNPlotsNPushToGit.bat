d6bb2628fa26ec0d48e924546f22e38fc24e8db9
Saideep Gajula <SGajula@slb.com>
2020-09-07 20:53:35 +0530

    Auto-push for 20hrs53min15secs_07-09-2020

--- COVID-19/RunToAddDataNLinksNPlotsNPushToGit.bat
+++ COVID-19/RunToAddDataNLinksNPlotsNPushToGit.bat
@@ -2,9 +2,9 @@
 echo script to RunToAddDataNLinksNPlots STARTED at %time:~0,2%hrs%time:~3,2%min%time:~6,2%secs
 set todaydatetime=%time:~0,2%hrs%time:~3,2%min%time:~6,2%secs_%date:~-7,2%-%date:~-10,2%-%date:~-4,4%
 echo Adding Data for Today
-python "C:\Users\SGajula\NotBackedUp\Me\Learn\GitHub\deepuHub\COVID-19\adddatafortoday.py" > "Logs\file_adddata_%todaydatetime%.txt"
+REM python "C:\Users\SGajula\NotBackedUp\Me\Learn\GitHub\deepuHub\COVID-19\adddatafortoday.py" > "Logs\file_adddata_%todaydatetime%.txt"
 echo Adding Links for Today
-python "C:\Users\SGajula\NotBackedUp\Me\Learn\GitHub\deepuHub\COVID-19\addtodaylinks.py" > "Logs\file_addlinks_%todaydatetime%.txt"
+REM python "C:\Users\SGajula\NotBackedUp\Me\Learn\GitHub\deepuHub\COVID-19\addtodaylinks.py" > "Logs\file_addlinks_%todaydatetime%.txt"
 timeout 15
 echo Generating Plots - Summary
 octave --no-gui "c:\users\sgajula\notbackedup\me\learn\github\deepuhub\covid-19\plotcovid2019.m"
